47038604e0e08f57e01649fd07e6de7fe6c1f22d
satvikb <borrasatvik@gmail.com>
2017-09-11 00:51:14 -0400

    init

---
+++ Letterama/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Letterama
+//
+//  Created by Satvik Borra on 9/11/17.
+//  Copyright © 2017 satvik borra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Letterama/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Letterama
+//
+//  Created by Satvik Borra on 9/11/17.
+//  Copyright © 2017 satvik borra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
